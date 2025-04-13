f9f7996f265b5cfab019123c76b1a7b8016277ab
AkmallNr <158866244+AkmallNr@users.noreply.github.com>
2025-04-13 13:01:46 +0700

    udah bisa deploy

--- app/src/main/java/com/example/schedo/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/schedo/network/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080/") // Emulator Android localhost
+            .baseUrl("https://4f58-103-57-37-16.ngrok-free.app/") // Emulator Android localhost
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
